59a3cb7bb3d69e2bec57ad06c5d8db30d108b7b9
Francis <shevy2k2@yahoo.com>
2024-07-09 15:36:23 +0300

    staging

--- resources/js/custom.js
+++ resources/js/custom.js
@@ -5,9 +5,12 @@ $("#contact-us").click(function () {
     let footer = document.getElementById('footer')
 
     footer.scrollIntoView({ 'behavior': 'smooth', 'block': 'end' });
-    $('#address').addClass('contact-address');
+    setTimeout(function () {
+        $('#address').addClass('contact-address');
+
+    }, 1000);
     setTimeout(function () {
         $('#address').removeClass('contact-address');
-    }, 5000);
+    }, 6000);
 
 }); 
